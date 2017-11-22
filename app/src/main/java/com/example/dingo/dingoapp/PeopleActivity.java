5081deda6de0ec5b057ec1584314145f365f1d64
wsherry <sherryguiwang@gmail.com>
2017-11-22 16:14:13 -0500

    PeopleActivityList
    -list of all users added

--- app/src/main/java/com/example/dingo/dingoapp/PeopleActivity.java
+++ app/src/main/java/com/example/dingo/dingoapp/PeopleActivity.java
@@ -2,6 +2,7 @@ package com.example.dingo.dingoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
 
 public class PeopleActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class PeopleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people);
+
+        public void onItemClick(AdapterView.OnItemClickListener()){
+            startActivity(new Intent(Users.this, Chat.class));
+    }
+
     }
 }
