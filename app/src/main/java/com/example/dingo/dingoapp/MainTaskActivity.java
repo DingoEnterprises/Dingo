fc6c23ebdcac0005f49c566de08978edc0a7e695
maximusyellow <yerrowranger@gmail.com>
2017-11-23 17:17:16 -0500

    Tabs and floating button
    Tabs are working, now trying to create viewtask, update task, and createtask pages

--- app/src/main/java/com/example/dingo/dingoapp/MainTaskActivity.java
+++ app/src/main/java/com/example/dingo/dingoapp/MainTaskActivity.java
@@ -1,67 +1,60 @@
 package com.example.dingo.dingoapp;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.example.dingo.dingoapp.CurrentTaskListFragment;
-import com.example.dingo.dingoapp.PastTaskListFragment;
-import com.example.dingo.dingoapp.R;
 
-/**
- * Created by Tair on 11/22/2017.
- */
+public class MainTaskActivity extends AppCompatActivity {
 
-public class MainTaskActivity extends AppCompatActivity implements CurrentTaskListFragment.OnFragmentInteractionListener, PastTaskListFragment.OnFragmentInteractionListener{
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
-        tabLayout.addTab(tabLayout.newTab().setText("Current Task List"));
-        tabLayout.addTab(tabLayout.newTab().setText("Past Task List"));
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Current"));
+        tabLayout.addTab(tabLayout.newTab().setText("Past"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return 2;
-            }
-
-            @Override
-            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-                return false;
-            }
-        };
-        viewPager.setAdapter((adapter));
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final FixedTabsPagerAdapter adapter = new FixedTabsPagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-            };
+            }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
 
-            };
+            }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 }
