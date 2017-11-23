6df0cf76f63cef851ec3f10a68fb71899c7fbfe9
wsherry <sherryguiwang@gmail.com>
2017-11-23 10:54:03 -0500

    Added code for People Activity

---
+++ app/src/main/java/com/example/dingo/dingoapp/UserInfo.java
@@ -0,0 +1,11 @@
+package com.example.dingo.dingoapp;
+
+/**
+ * Created by Sherry Wang on 2017-11-23.
+ */
+
+public class UserInfo {
+    static String user = "";
+    static String pass = "";
+    static String chatWith = "";
+}
