4f5365714ab95254c643b9370bdf46a1ad800285
Ash Labelle <alabe101@uottawa.ca>
2017-12-04 13:32:08 -0500

    Revert "Revert "fixed error""
    This reverts commit 7db938392c1c08ad38db70aab699675507b98cca.

---
+++ app/src/main/java/com/example/dingo/dingoapp/Household.java
@@ -0,0 +1,13 @@
+package com.example.dingo.dingoapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ajrsa on 2017-12-03.
+ */
+
+public class Household {
+    private ArrayList<String> emails;
+    public String name;
+
+}
